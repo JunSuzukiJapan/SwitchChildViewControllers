ceaa5032f871092932f1cd0af6e07929e8569215
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2018-03-03 13:28:17 +0900

    Initial Commit

---
+++ SwitchChildViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitchChildViewControllers
+//
+//  Created by jun suzuki on 2018/03/03.
+//  Copyright © 2018年 jun suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
